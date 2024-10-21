993046f551dade26ad67eeb8d5af8fbf71b34a78
Ksenia-Kryukova <ksyusha-kryukova@bk.ru>
2024-10-21 16:52:54 +0200

    1.Адаптированы классы из entities для SQLAlchemy. 2.Резлизован класс SqlDb, cоответсвующий интерфейсу Db.

--- pizza-store/src/model/db.py
+++ pizza-store/src/model/db.py
@@ -1,4 +1,5 @@
 from .entities import User, Order, Pizza, Topping, BasePizza
+from sqlalchemy.orm import Session
 
 
 # Interface for db
@@ -68,3 +69,39 @@ class InMemDb(Db):
     def save_base_pizza(self, base_pizza: BasePizza):
         if base_pizza.base_pizza_id not in self.base_pizzas:
             self.base_pizzas[base_pizza.base_pizza_id] = base_pizza
+
+
+class SqlDb(Db):
+    def __init__(self, session: Session):
+        self.session = session
+
+    def find_user(self, user_id: str) -> User:
+        return self.session.query(User).filter(User.user_id == user_id).one_or_none()
+
+    def find_order(self, order_id: str) -> Order:
+        return self.session.query(Order).filter(Order.order_id == order_id).one_or_none()
+
+    def find_pizza(self, pizza_id: str) -> Pizza:
+        return self.session.query(Pizza).filter(Pizza.pizza_id == pizza_id).one_or_none()
+
+    def find_topping(self, topping_id: str) -> Topping:
+        return self.session.query(Topping).filter(Topping.topping_id == topping_id).one_or_none()
+
+    def find_base_pizza(self, base_pizza_id: str) -> BasePizza:
+        return self.session.query(BasePizza).filter(BasePizza.base_pizza_id == base_pizza_id).one_or_none()
+
+    def add_user(self, user: User):
+        self.session.add(user)
+        self.session.commit()
+
+    def save_order(self, order: Order):
+        self.session.add(order)
+        self.session.commit()
+
+    def save_topping(self, topping: Topping):
+        self.session.add(topping)
+        self.session.commit()
+
+    def save_base_pizza(self, base_pizza: BasePizza):
+        self.session.add(base_pizza)
+        self.session.commit()
--- pizza-store/src/model/entities.py
+++ pizza-store/src/model/entities.py
@@ -1,5 +1,12 @@
 from enum import Enum
 from typing import List
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, Integer, Float, Boolean, ARRAY, ForeignKey
+from sqlalchemy.orm import relationship
+from typing import Any
+
+
+Base: Any = declarative_base()
 
 
 class OrderStatus(Enum):
@@ -12,16 +19,29 @@ class OrderStatus(Enum):
     COMPLETED = 7
 
 
-class User:
+class User(Base):
     """Класс для хранения информации о заказчике"""
+    __tablename__ = 'users'
+    user_id = Column(String(20), primary_key=True)
+    name = Column(String(50))
+    phone_number = Column(Integer)
+
     def __init__(self, user_id: str, name: str, phone_number: int):
         self.user_id = user_id
         self.name = name
         self.phone_number = phone_number
 
 
-class Order:
+class Order(Base):
     """Класс для хранения информации о заказе"""
+    __tablename__ = 'orders'
+    order_id = Column(String(20), primary_key=True)
+    user_id = Column(String(20))
+    pizzas = relationship("Pizza", secondary="order_pizza")
+    address = Column(String(100))
+    order_status = Column(Integer, default=OrderStatus.NEW.value)
+    paid = Column(Boolean, default=False)
+
     def __init__(self, order_id: str, user_id: str, pizzas: List['Pizza'], address: str):
         self.order_id = order_id
         self.user_id = user_id
@@ -31,8 +51,21 @@ class Order:
         self.paid = False
 
 
-class BasePizza:
+class OrderPizza(Base):
+    """Вспомогательная таблица для связи заказов и пицц"""
+    __tablename__ = 'order_pizza'
+    order_id = Column(String(20), ForeignKey('orders.order_id'), primary_key=True)
+    pizza_id = Column(String(20), ForeignKey('pizzas.pizza_id'), primary_key=True)
+
+
+class BasePizza(Base):
     """Представляет основную пиццу из меню с её свойствами"""
+    __tablename__ = 'base_pizzas'
+    base_pizza_id = Column(String(20), primary_key=True)
+    name = Column(String(50))
+    description = Column(String(255))
+    price_rub = Column(Float)
+
     def __init__(self, base_pizza_id: str, name: str, description: str, price_rub: float):
         self.base_pizza_id = base_pizza_id
         self.name = name
@@ -40,16 +73,27 @@ class BasePizza:
         self.price_rub = price_rub
 
 
-class Pizza:
+class Pizza(Base):
     """Представляет пиццу для заказа (основная пицца из меню + дополнительные ингредиенты)"""
+    __tablename__ = 'pizzas'
+    pizza_id = Column(String(20), primary_key=True)
+    base_pizza_id = Column(String(20))
+    topping_ids = Column(ARRAY(String(20)))
+
     def __init__(self, pizza_id: str, base_pizza_id: str, topping_ids: List[str]):
         self.pizza_id = pizza_id
         self.base_pizza_id = base_pizza_id
         self.topping_ids = topping_ids
 
 
-class Topping:
+class Topping(Base):
     """Класс для работы с дополнительными ингредиентами к пицце"""
+    __tablename__ = 'toppings'
+    topping_id = Column(String(20), primary_key=True)
+    name = Column(String(50))
+    description = Column(String(255))
+    price_rub = Column(Float)
+
     def __init__(self, topping_id: str, name: str, description: str, price_rub: float):
         self.topping_id = topping_id
         self.name = name
